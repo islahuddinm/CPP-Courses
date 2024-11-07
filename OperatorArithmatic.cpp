125326f8be768926263891b4d4d20078af72f664
islahuddinm <pesonaalamdunia@gmail.com>
2024-11-07 10:22:16 +0700

    Updated Commit

--- OperatorArithmatic.cpp
+++ OperatorArithmatic.cpp
@@ -26,12 +26,20 @@ int main() {
     //students/=2;
 
     int reminder = students % 3; //modulus (a mathematical operator that returns the remainder(rest) of a division operation. it is integer reminder)
+    int reminder2 = 3 % 20; //result: 3
+    int reminder3 = 5 % 20; //result: 5
+    int reminder4 = 6 % 15; //result: 6
+    int reminder5 = 0 % 2; //result: 0
 
     //int teacher = 6 - 5 + 4 * 3 / 2;
     //int teachers = 6 - (5 + 4) * 3 / 2;
 
     //std::cout << students << '\n';
     std::cout << reminder << '\n'; //result 1 not 2 because 20 students divided 2 in the beginning
+    std::cout << reminder2 << '\n'; //result: 3
+    std::cout << reminder3 << '\n'; //result: 5
+    std::cout << reminder4 << '\n'; //result: 6
+    std::cout << reminder5 << '\n'; //result: 0
     //std::cout << teacher << '\n';
     //std::cout << teachers;
 
