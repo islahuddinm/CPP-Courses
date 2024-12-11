2ce23df83e45ab1950a13db2725c8f56953ec1e1
Muhammad Islahuddin <pesonaalamdunia@gmail.com>
2024-12-11 12:05:35 +0700

    Initial Commit

---
+++ DesignPattern.cpp
@@ -0,0 +1,56 @@
+/*
+
+Design patterns are typical solutions to commonly occurring problems in software design.
+They are like pre-made blueprints/templates that you can customize to solve a recurring design problem
+in your code. It's not a specific piece of code but a template or concept for
+solving problems that can be adapted to various situations in programming.
+
+You can’t just find a pattern and copy it into your program, the way you can do with
+off-the-shelf(Siap pakai) functions or libraries. The pattern is not a specific piece of code,
+but a general concept for solving a particular problem.
+
+
+a programming design pattern is a template for solving a common problem, while
+a programming paradigm is a way to structure a computer program.
+
+
+Design patterns can be used in different programming paradigms, such as object-oriented and
+functional programming. For example, in procedural programming, design patterns like modules,
+decorators, and commands can be used to solve problems.
+
+
+Example:
+Object-Oriented Programming Paradigm (OOP)
+Paradigm Principles:
+1. Code is organized around objects, which represent real-world entities.
+2. Objects encapsulate data (attributes) and behavior (methods).
+3. Key concepts: Encapsulation, Inheritance, Polymorphism, and Abstraction.
+
+
+Types of Design Patterns:
+1. Creational Patterns:
+    Focus on object creation mechanisms.
+    
+    Example: Singleton, Factory, Abstract Factory, Builder, Prototype.
+
+2. Structural Patterns:
+    Deal with object composition and relationships.
+    Example: Adapter, Bridge, Composite, Decorator, Facade, Flyweight, Proxy.
+
+3. Behavioral Patterns:
+    Concerned(Berkaitan) with object interactions and delegation  of responsibilities ("delegation" refers to
+    the practice of one object passing on a task or responsibility to another object, essentially allowing
+    an object to "delegate" work to another object that is better suited to handle it,
+    instead of performing the task itself).
+
+    Example: Observer, Strategy, Command, Chain of Responsibility, State, Template Method,
+    Visitor, Mediator, Interpreter, Iterator, memento.
+
+*/
+
+#include <iostream>
+
+int main() {
+
+    return 0;
+}
