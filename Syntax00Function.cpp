acf7f691560284de2cdd97e9e351cf8d54e028e1
migame25 <islahuddinm001@gmail.com>
2026-06-18 08:00:21 +0700

    Initial Commit

---
+++ Syntax00Function.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// Syntax refers to the set of rules that govern how words and phrases are arranged to create well-formed, meaningful sentences.
+// Functions in programming are modular units of code designed to perform specific tasks.
+
+int main() {
+
+    return 0;
+}
---
+++ Syntax01Variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// Syntax refers to the set of rules that govern how words and phrases are arranged to create well-formed, meaningful sentences.
+// Variable = symbolic name assigned to store data and value.
+
+int main() {
+
+    int x = 1; // Variable
+
+    return 0;
+}
