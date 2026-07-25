9376eaa83fcffe17af14917b1cfa34fbda45c48d
migame25 <islahuddinm001@gmail.com>
2026-07-25 15:29:57 +0700

    Initial Commit

---
+++ ImageCreation/Filter.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+
+int main() {
+
+    std::ifstream image;
+    std::ofstream newimage;
+
+    image.open("PPMimages/Monument.ppm");
+    newimage.open("FilterMonument.ppm");
+
+    if (!image.is_open()) {
+        std::cout << "Error opening file" << std::endl;
+        return 1;
+    } else {
+
+        std::string type = "", width = "", height = "", RGB = "";
+        image >> type;
+        image >> width;
+        image >> height;
+        image >> RGB;
+
+        // std::cout << type << width << height << RGB << std::endl;
+
+        newimage << type << std::endl;
+        newimage << width << " " << height << std::endl;
+        newimage << RGB << std::endl;
+
+        std::string red = "", green = "", blue = "";
+        int r = 0, g = 0, b = 0;
+
+        while (!image.eof()) {
+            image >> red;
+            image >> green;
+            image >> blue;
+
+            std::stringstream redstream(red);
+            std::stringstream greenstream(green);
+            std::stringstream bluestream(blue);
+
+            redstream >> r;
+            greenstream >> g;
+            bluestream >> b;
+
+            if(b + 50 >= 255) {
+                b = 255;
+            } else {
+                b += 50;
+            }
+
+            newimage << r << " " << g << " " << b << std::endl;
+        }
+    }
+
+    image.close();
+    newimage.close();
+}
